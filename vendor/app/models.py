f814b12c702d38e63b3a6f0c69e50bd64533bed0
Pongsakorn Samotong <ikkyu@Pongsakorns-MacBook-Pro.local>
2023-01-20 13:41:59 +0700

    fix import

--- vendor/app/models.py
+++ vendor/app/models.py
@@ -16,20 +16,21 @@ class Product(models.Model):
     name = models.CharField(max_length=MAX_CHAR_LENGHT)
     price = models.DecimalField(max_digits=MAX_DIGIT, decimal_places=MAX_DECIMAL_PLACE, default=DEFAULT_DECIMAL_PLACE)
 
-class Stock(models.Model):
-    id = models.AutoField(primary_key=True)
-    class Meta:
-        constraints = [
-          models.UniqueConstraint(fields=['machine', 'product'], name='machine_product')
-        ]
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-
 class Machine(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=MAX_CHAR_LENGHT)
     location = models.CharField(max_length=MAX_CHAR_LENGHT)
     is_active = models.BooleanField(default=True)
+
+class Stock(models.Model):
+    id = models.AutoField(primary_key=True)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    class Meta:
+        constraints = [
+          models.UniqueConstraint(fields=['machine', 'product'], name='machine_product')
+        ]
 
 
 
